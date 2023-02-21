a48735912ebd61d12710f3c3b8497f350217b48e
Monica <monica.dominog@gmail.com>
2023-02-20 22:17:25 -0300

    Add complete project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -20,8 +22,36 @@ public class Main {
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMonica = new Dev();
+        devMonica.setNome("Monica");
+        devMonica.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Monica: " + devMonica.getConteudosInscritos());
+        System.out.println("..........Progresso de Monica");
+        devMonica.progredir();
+        devMonica.progredir();
+        System.out.println("Conteudos inscritos de Monica: " + devMonica.getConteudosInscritos());
+        System.out.println("Conteudos concluidos de Monica: " + devMonica.getConteudosConcluidos());
+        System.out.println("XP: " + devMonica.calcularTotalXp());
+
+        System.out.println("**** PROXIMO DEV ******");
+        Dev devMurilo = new Dev();
+        devMurilo.setNome("Murilo");
+        devMurilo.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos inscritos de Murilo: " + devMurilo.getConteudosInscritos());
+        devMurilo.progredir();
+        devMurilo.progredir();
+        devMurilo.progredir();
+        System.out.println("..........Progresso de Murilo");
+        System.out.println("Conteudos inscritos de Murilo: " + devMurilo.getConteudosInscritos());
+        System.out.println("Conteudos concluidos de Murilo: " + devMurilo.getConteudosConcluidos());
+        System.out.println("XP: " + devMurilo.calcularTotalXp());
+
     }
 }
